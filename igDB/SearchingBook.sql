62f2d0b7a91eb2b226f8dd420bbba2b6c2072fd4
Wojtini <56893313+Wojtini@users.noreply.github.com>
2021-01-12 18:56:50 +0100

    Triggers, Searching Procedure (prototype)
    Still needs to check LeftJoin

---
+++ igDB/SearchingBook.sql
@@ -0,0 +1,61 @@
+DROP PROCEDURE IF EXISTS GetSearchResults;
+DELIMITER //
+
+CREATE PROCEDURE GetSearchResults(IN title VARCHAR(120),IN author VARCHAR(120),IN category VARCHAR(120),IN priceOrder INT,IN ratingOrder INT)
+BEGIN
+	DECLARE orderByText VARCHAR(120) DEFAULT "";
+	#Creating Sort with Concat
+	#Price
+	IF priceOrder = 1 AND ratingOrder = 1 THEN
+		SET orderByText = " ORDER BY sredniaOcena ASC, ksiazki.cena ASC priceText;";
+	END IF;
+	IF priceOrder = -1 AND ratingOrder = -1 THEN
+		SET orderByText = " ORDER BY sredniaOcena DESC, ksiazki.cena DESC priceText;";
+	END IF;
+	IF priceOrder = 1 AND ratingOrder = -1 THEN
+		SET orderByText = " ORDER BY sredniaOcena DESC, ksiazki.cena ASC priceText;";
+	END IF;
+	IF priceOrder = -1 AND ratingOrder = 1 THEN
+		SET orderByText = " ORDER BY sredniaOcena ASC, ksiazki.cena DESC priceText;";
+	END IF;
+	IF priceOrder = 0 AND ratingOrder = 0 THEN
+		SET orderByText = ";";
+	END IF;
+	IF priceOrder = 0 AND ratingOrder = 1 THEN
+		SET orderByText = " ORDER BY sredniaOcena ASC;";
+	END IF;
+	IF priceOrder = 0 AND ratingOrder = -1 THEN
+		SET orderByText = " ORDER BY sredniaOcena ASC;";
+	END IF;
+	IF priceOrder = 1 AND ratingOrder = 0 THEN
+		SET orderByText = " ORDER BY ksiazki.cena ASC;";
+	END IF;
+	IF priceOrder = -1 AND ratingOrder = 0 THEN
+		SET orderByText = " ORDER BY ksiazki.cena DESC;";
+	END IF;
+	
+	#Left join
+	PREPARE STMT1 FROM
+	#SELECT
+	CONCAT(
+	'
+		SELECT tytul,cena,ilosc,imie,nazwisko,nazwa,AVG(iloscgwiazdek) AS sredniaOcena FROM ksiazki
+		JOIN autorzy
+		JOIN kategorie
+		JOIN recenzje
+		WHERE 
+		autorzy.id = ksiazki.autor AND
+		kategorie.id = ksiazki.kategoria AND
+		recenzje.id_ksiazka = ksiazki.id AND
+		tytul LIKE CONCAT("%",?,"%") AND
+		kategorie.nazwa LIKE CONCAT("%",?,"%")AND
+		CONCAT(autorzy.imie," ",autorzy.nazwisko) LIKE CONCAT("%",?,"%")
+		GROUP BY ksiazki.id
+	',orderByText);
+	
+	EXECUTE STMT1 USING title,category,author;
+END //
+
+DELIMITER ;
+
+CALL GetSearchResults("","","",0,0);
---
+++ igDB/Triggers.sql
@@ -0,0 +1,40 @@
+###Triggery przed usunięciem autora
+
+#usunięcie ksiazki wybranego autora
+DROP TRIGGER IF EXISTS usun_autor_ksiazki;
+CREATE TRIGGER usun_autor_ksiazki
+	BEFORE DELETE ON autorzy
+	FOR EACH ROW
+DELETE FROM ksiazki WHERE ksiazki.autor=OLD.id;
+
+###Triggery przed usunięciem książki (potrzebne do usunięcia autora)
+
+#usuwanie recenzji danej ksiazki
+DROP TRIGGER IF EXISTS usun_ksiazka_recenzja;
+CREATE TRIGGER usun_ksiazka_recenzja
+	BEFORE DELETE ON ksiazki
+	FOR EACH ROW
+DELETE FROM recenzje WHERE recenzje.id_ksiazka=OLD.id;
+
+#usuwanie produktow zamowien z ta ksiazka
+DROP TRIGGER IF EXISTS usun_ksiazka_produkt;
+CREATE TRIGGER usun_ksiazka_produkt
+	BEFORE DELETE ON ksiazki
+	FOR EACH ROW
+DELETE FROM produktyzamowien WHERE produktyzamowien.id_ksiazka=OLD.id;
+
+###Triggery przed usunięciem użytkownika
+
+#usuwanie recenzji
+DROP TRIGGER IF EXISTS usun_uzytkownik_recenzje;
+CREATE TRIGGER usun_uzytkownik_recenzje
+	BEFORE DELETE ON uzytkownicy
+	FOR EACH ROW
+DELETE FROM recenzje WHERE recenzje.id_uzytkownik=OLD.id;
+
+#usuwanie zamowien
+DROP TRIGGER IF EXISTS usun_uzytkownik_zamowienia;
+CREATE TRIGGER usun_uzytkownik_zamowienia
+	BEFORE DELETE ON uzytkownicy
+	FOR EACH ROW
+DELETE FROM zamowienia WHERE zamowienia.id_uzytkownik=OLD.id;
